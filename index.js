5f1fed629e0763f9f620d8f4c8fb6ae4feea83a2
Ibrian Giorgio <ibrian.giorgio@gmail.com>
2025-07-27 22:35:15 -0300

    feat: add multer and files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,8 +13,24 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+/*
+2. Puedes enviar un formulario que incluya una carga de archivo.
+Esperando:3. El campo de entrada del archivo de formulario tiene el atributo name establecido en upfile.
+Esperando:4. Cuando envíes un archivo, recibirás él name, type y 
+size del archivo en bytes dentro de la respuesta JSON.
 
+*/
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
